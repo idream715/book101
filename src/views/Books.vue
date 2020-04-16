57820b84b37320a54e94da446e886f208e8657c0
iTOUCH41 <suratach0072@gmail.com>
2020-04-16 10:12:43 +0100

    6

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="about">
-    <h1>This is an books page</h1>
+      <v-container>
+        <v-row no-gutters="">
+          <v-col 
+          v-for="n in 47"
+          :key="n"
+          
+          >
+            <v-card class="mx-auto mt-3 mb-5" max-width="180">
+              <v-img src="https://book.rgtcenter.com/cover/052-01.jpg" height="250px"></v-img>
+              <v-card-title>
+                <v-title class="text-truncate">
+                  ที่นี่มีคำตอบ ที่นี่มีคำตอบ
+                </v-title>
+              </v-card-title>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     <sarabun msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
